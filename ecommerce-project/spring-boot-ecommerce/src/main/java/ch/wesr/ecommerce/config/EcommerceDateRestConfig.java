b03b987f4984fdcc7af45351ff47c4cf503ba7f3
Rene Weishaupt <rene.weishaupt@thurweb.ch>
2020-02-16 21:56:23 +0100

    .gitignore and disable httpMethods put, post, delete

--- ecommerce-project/spring-boot-ecommerce/src/main/java/ch/wesr/ecommerce/config/EcommerceDateRestConfig.java
+++ ecommerce-project/spring-boot-ecommerce/src/main/java/ch/wesr/ecommerce/config/EcommerceDateRestConfig.java
@@ -1,6 +1,7 @@
 package ch.wesr.ecommerce.config;
 
 import ch.wesr.ecommerce.entity.Product;
+import ch.wesr.ecommerce.entity.ProductCategory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -9,11 +10,18 @@ import org.springframework.http.HttpMethod;
 @Configuration
 public class EcommerceDateRestConfig implements RepositoryRestConfigurer {
 
-    public void configureRespositoryRestConfiguration(RepositoryRestConfiguration config) {
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
         HttpMethod[] theUnsupportedActions = {HttpMethod.DELETE, HttpMethod.POST, HttpMethod.PUT};
 
+        // disable Http Methods for Product
         config.getExposureConfiguration()
                 .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        // disable Http Methods for ProductCategory
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
                 .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
                 .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
     }
