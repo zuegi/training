ede7307cb4eda0bbd9357154864d57fa8d970f2c
Rene Weishaupt <rene.weishaupt@thurweb.ch>
2020-02-17 21:08:18 +0100

    new angular-ecommerce front end with product list

---
+++ ecommerce-project/angular-ecommerce/src/app/common/product.ts
@@ -0,0 +1,11 @@
+export class Product {
+  siku: string;
+  name: string;
+  description: string;
+  unitPrice: number;
+  imageUrl: string;
+  active: boolean;
+  unitsInStock: number;
+  dateCreated: Date;
+  lastUpdated: Date;
+}
--- ecommerce-project/spring-boot-ecommerce/src/main/java/ch/wesr/ecommerce/dao/ProductRepository.java
+++ ecommerce-project/spring-boot-ecommerce/src/main/java/ch/wesr/ecommerce/dao/ProductRepository.java
@@ -2,6 +2,8 @@ package ch.wesr.ecommerce.dao;
 
 import ch.wesr.ecommerce.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
