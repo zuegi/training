74c7f1e6fb1a56d595c1e913dae3c66fc1c017bb
Rene Weishaupt <rene.weishaupt@thurweb.ch>
2020-05-08 13:38:23 +0200

    zuegi - groot

--- flowable/holidayrequest/src/main/java/ch/wesr/flowable/holidayrequest/HolidayRequest.java
+++ flowable/holidayrequest/src/main/java/ch/wesr/flowable/holidayrequest/HolidayRequest.java
@@ -1,15 +1,15 @@
 package ch.wesr.flowable.holidayrequest;
 
-import org.flowable.engine.ProcessEngine;
-import org.flowable.engine.ProcessEngineConfiguration;
-import org.flowable.engine.RepositoryService;
-import org.flowable.engine.RuntimeService;
+import org.flowable.engine.*;
+import org.flowable.engine.history.HistoricActivityInstance;
 import org.flowable.engine.impl.cfg.StandaloneProcessEngineConfiguration;
 import org.flowable.engine.repository.Deployment;
 import org.flowable.engine.repository.ProcessDefinition;
 import org.flowable.engine.runtime.ProcessInstance;
+import org.flowable.task.api.Task;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -17,20 +17,63 @@ public class HolidayRequest {
 
     public static final String HOLIDAY_REQUEST_BPMN_20_XML = "holiday-request.bpmn20.xml";
     private static RepositoryService repositoryService;
+    private static ProcessEngine processEngine;
+    private static Scanner scanner;
+    private static Map<String, Object> variables;
 
     public static void main(String[] args) {
-        ProcessEngine processEngine = configureProcessEngine();
-        Deployment deployment =deploy(processEngine, HOLIDAY_REQUEST_BPMN_20_XML);
+        processEngine = configureProcessEngine();
+        Deployment deployment =deploy(HOLIDAY_REQUEST_BPMN_20_XML);
         checkProcessDefinition(deployment);
 
         RuntimeService runtimeService  = processEngine.getRuntimeService();
         ProcessInstance processInstance =
                 runtimeService.startProcessInstanceByKey("holidayRequest", initializeProcessVariable());
         // hier stehe ich https://flowable.com/open-source/docs/bpmn/ch02-GettingStarted/#sidetrack-transactionality
+        askForTheTaskToComplete();
+        showHistory(processInstance);
+    }
+
+    private static void showHistory(ProcessInstance processInstance) {
+        HistoryService historyService = processEngine.getHistoryService();
+        List<HistoricActivityInstance> activities =
+                historyService.createHistoricActivityInstanceQuery()
+                        .processInstanceId(processInstance.getId())
+                        .finished()
+                        .orderByHistoricActivityInstanceEndTime().asc()
+                        .list();
+
+        for (HistoricActivityInstance activity : activities) {
+            System.out.println(activity.getActivityId() + " took "
+                    + activity.getDurationInMillis() + " milliseconds");
+        }
+    }
+    private static void askForTheTaskToComplete() {
+
+        TaskService taskService = processEngine.getTaskService();
+        List<Task> tasks = taskService.createTaskQuery().taskCandidateGroup("managers").list();
+        System.out.println("You have " + tasks.size() + " tasks:");
+        for (int i=0; i<tasks.size(); i++) {
+            System.out.println((i+1) + ") " + tasks.get(i).getName());
+        }
+
+        System.out.println("Which task would you like to complete?");
+        int taskIndex = Integer.valueOf(scanner.nextLine());
+        Task task = tasks.get(taskIndex - 1);
+        Map<String, Object> processVariables = taskService.getVariables(task.getId());
+        System.out.println(processVariables.get("employee") + " wants " +
+                processVariables.get("nrOfHolidays") + " of holidays. Do you approve this?");
+
+
+        boolean approved = scanner.nextLine().toLowerCase().equals("y");
+        variables = new HashMap<String, Object>();
+        variables.put("approved", approved);
+        taskService.complete(task.getId(), variables);
+
     }
 
     private static Map<String, Object> initializeProcessVariable() {
-        Scanner scanner= new Scanner(System.in);
+        scanner= new Scanner(System.in);
 
         System.out.println("Who are you?");
         String employee = scanner.nextLine();
@@ -41,7 +84,7 @@ public class HolidayRequest {
         System.out.println("Why do you need them?");
         String description = scanner.nextLine();
 
-        Map<String, Object> variables = new HashMap<String, Object>();
+        variables = new HashMap<String, Object>();
         variables.put("employee", employee);
         variables.put("nrOfHolidays", nrOfHolidays);
         variables.put("description", description);
@@ -66,7 +109,7 @@ public class HolidayRequest {
         return cfg.buildProcessEngine();
     }
 
-    private static Deployment deploy(ProcessEngine processEngine, String file) {
+    private static Deployment deploy(String file) {
         repositoryService = processEngine.getRepositoryService();
         return repositoryService.createDeployment()
                 .addClasspathResource(file)
