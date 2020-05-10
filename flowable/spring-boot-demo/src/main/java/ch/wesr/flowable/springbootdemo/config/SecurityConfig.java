dc77572a283429121e25d1bb1580bfd361852d49
Rene Weishaupt <rene.weishaupt@thurweb.ch>
2020-05-10 13:04:08 +0200

    zuegi - h2 console is visible now - though security almost disabled

--- flowable/spring-boot-demo/src/main/java/ch/wesr/flowable/springbootdemo/config/SecurityConfig.java
+++ flowable/spring-boot-demo/src/main/java/ch/wesr/flowable/springbootdemo/config/SecurityConfig.java
@@ -4,9 +4,13 @@ import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointR
 import org.springframework.boot.actuate.health.HealthEndpoint;
 import org.springframework.boot.actuate.info.InfoEndpoint;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 
 @Configuration
@@ -17,21 +21,35 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/h2-console/**").permitAll()  // h2-console permitted for development purpose only
-                .requestMatchers(EndpointRequest.to(InfoEndpoint.class, HealthEndpoint.class)).permitAll()
-                .requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ACTUATOR")
-                .antMatchers("/api/**").hasRole("REST")
-                .antMatchers("/**").authenticated()
-                .and()
-                .formLogin()
-                .and()
-                .httpBasic()
-                .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().csrf().ignoringAntMatchers("/h2-console/**")//don't apply CSRF protection to /h2-console
-                .and()
-                .csrf().disable();
+                .antMatchers("/h2-console/**").permitAll()  // h2-console permitted
+        ;
+        http.csrf().ignoringAntMatchers("/h2-console/**");//don't apply CSRF protection to /h2-console
+        http.csrf().disable(); //don't apply CSRF protection
+
+        //  Refused to display 'http://localhost:8080/xxx/xxx/upload-image?CKEditor=text&CKEditorFuncNum=1&langCode=ru'
+        //  in a frame because it set 'X-Frame-Options' to 'DENY'.
+        http.headers().frameOptions().sameOrigin();
+        // keine Sessions
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        // cors
+        http.cors().configurationSource(corsConfiguratonSource());
     }
 
+    private CorsConfigurationSource corsConfiguratonSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration().applyPermitDefaultValues();
+//        corsConfiguration.setAllowedOrigins(frontendUrls);
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedMethod(HttpMethod.PUT);
+        corsConfiguration.addAllowedMethod(HttpMethod.POST);
+        corsConfiguration.addAllowedMethod(HttpMethod.DELETE);
+        corsConfiguration.addAllowedMethod(HttpMethod.GET);
+        corsConfiguration.addAllowedMethod(HttpMethod.OPTIONS);
+        corsConfiguration.addAllowedMethod(HttpMethod.HEAD);
+        corsConfiguration.addAllowedMethod(HttpMethod.PATCH);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
+    }
 
 }
