67b0a730a2f37ce657f538e01e97a4bfd18b4186
Rene Weishaupt <rene.weishaupt@thurweb.ch>
2020-05-13 08:48:33 +0200

    zuegi - I am not able to succeed this Test or pass variable to Human Task

--- flowable/spring-boot-demo/src/test/java/ch/wesr/flowable/springbootdemo/OnBoardingCaseTest.java
+++ flowable/spring-boot-demo/src/test/java/ch/wesr/flowable/springbootdemo/OnBoardingCaseTest.java
@@ -18,8 +18,11 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Slf4j
 public class OnBoardingCaseTest extends SpringBootDemoTestConfiguration {
@@ -39,13 +42,20 @@ public class OnBoardingCaseTest extends SpringBootDemoTestConfiguration {
         map.put("prospectName", "Paul");
         map.put("initiator", "flowfest");
 
-        // then
+        // then case wird gestartet und die Prospect Application / Prozess sofort ausgeführt
         CaseInstance caseInstance = cmmnRuntimeService.createCaseInstanceBuilder()
                 .caseDefinitionKey("onBoardingTM")
                 .variables(map)
                 .start();
 
         Assertions.assertNotNull(caseInstance);
+
+        // das verstehe ich schon nicht, die case Variablens sind hier leer
+        Map<String, Object> caseVariables = caseInstance.getCaseVariables();
+        log.info("caseVariables: {}"); // <- leer
+        // also muss man die Variablen so abholen
+        Map<String, Object> variables = cmmnRuntimeService.getVariables(caseInstance.getId());
+        log.info("caseVariables über cmmnRuntimeService: {}", variables);
 
         List<Task> list = taskService.createTaskQuery().active().list();
         assertThat(list, hasSize(2));
@@ -54,7 +64,8 @@ public class OnBoardingCaseTest extends SpringBootDemoTestConfiguration {
         Task enhanceWithSwiftKeyTask = list.stream()
                 .filter(task -> task.getTaskDefinitionKey().equals("enhanceWithSwiftKey")).map(task -> task).collect(Collectors.toList()).get(0);
 
-        log.info("formKey: {}", enhanceWithSwiftKeyTask.getFormKey());
+        Map<String, Object> enhanceWithSwiftTaskVariables = taskService.getVariables(enhanceWithSwiftKeyTask.getId());
+        log.info("Enhance Swift Task Variables: {}", enhanceWithSwiftTaskVariables.toString());
 
         seeWhatHappens(caseInstance);
 
