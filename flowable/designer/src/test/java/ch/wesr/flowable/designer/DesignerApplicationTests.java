606e7d1580b9985264f79ae6654a8b08ea3b2fff
Rene Weishaupt <rene.weishaupt@thurweb.ch>
2020-05-27 15:45:54 +0200

    zuegi/groot - designer

---
+++ flowable/designer/src/test/java/ch/wesr/flowable/designer/DesignerApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.wesr.flowable.designer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
